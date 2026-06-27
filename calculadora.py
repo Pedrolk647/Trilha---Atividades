e7a471291077154e8c3f93010ef10f52ddff6f60
Pedro Luís <pedrolk647@gmail.com>
2026-06-27 19:07:58 -0300

    Add files via upload

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+def calculadora(a,b):
+    return a+b
+
+def multiplicacao(a,b):
+    return a*b
+
+if __name__ == '__main__':
+
+   print(f"Result: {calculadora(1,2)}")
+
+
---
+++ main.py
@@ -0,0 +1,4 @@
+import calculadora
+
+print(f"O resultado da soma é: {calculadora.soma(1,2)}")
+print(f"O resultado da soma é: {calculadora.multiplicacao(1,2)}")
